1fb895e2cd651b981516b0a2d91bf603064f5121
Anton Nikulin <tsh@users.noreply.github.com>
2017-08-14 19:49:24 +0300

    Update fibd.py

--- bioinformatics-stronghold/fibd.py
+++ bioinformatics-stronghold/fibd.py
@@ -1,14 +1,22 @@
+"""
+Given: Positive integers n≤100 and m≤20.
+Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for m months.
+"""
+
 N = 6  # number of months
 M = 3  # ttl
 
-# 1 1 2 2 3 4
-
-memo = [0 for _ in range(N+1)]
+memo = [None for _ in range(N)]
+memo[0] = 1
 memo[1] = 1
-memo[2] = 1
 
-for month in range(3, N+1):
-    dead = 0 if month < M else memo[month-M]
-    memo[month] = (memo[month-1] + memo[month-2]) - dead
+for month in range(2, N):
+    if month < M:
+        dead = 0
+    elif month == M:
+        dead = memo[0]  # here (month - (M+1)) < 0 fix this be taking first value
+    else:
+        dead = memo[month - (M+1)]
+    memo[month] = (memo[month - 1] + memo[month - 2]) - dead
 
-print memo
+print memo[-1]
