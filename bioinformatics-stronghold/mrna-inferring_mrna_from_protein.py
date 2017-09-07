5267e4cf50de547b4f9bb0a0f934c4a1a443f0c6
Anton Nikulin <tsh@users.noreply.github.com>
2017-09-07 18:08:54 +0300

    Create mrna-inferring_mrna_from_protein.py

---
+++ bioinformatics-stronghold/mrna-inferring_mrna_from_protein.py
@@ -0,0 +1,30 @@
+"""
+Given: A protein string of length at most 1000 aa.
+Return: The total number of different RNA strings from which the protein could have been translated,
+modulo 1,000,000. (Don't neglect the importance of the stop codon in protein translation.)
+"""
+
+from collections import defaultdict
+
+codon_table = {
+    'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V', 'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V', 'UUA': 'L',
+    'CUA': 'L', 'AUA': 'I', 'GUA': 'V', 'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V', 'UCU': 'S', 'CCU': 'P',
+    'ACU': 'T', 'GCU': 'A', 'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A', 'UCA': 'S', 'CCA': 'P', 'ACA': 'T',
+    'GCA': 'A', 'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A', 'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D', 'UAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E', 'UAG': 'Stop',
+    'CAG': 'Q', 'AAG': 'K', 'GAG': 'E', 'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G', 'UGC': 'C', 'CGC': 'R',
+    'AGC': 'S', 'GGC': 'G', 'UGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G', 'UGG': 'W', 'CGG': 'R',
+    'AGG': 'R', 'GGG': 'G',
+}
+
+prot_to_rna = defaultdict(list)
+for rna, prot in codon_table.items():
+    prot_to_rna[prot].append(rna)
+
+inp = 'MA'
+res = 1
+for aa in inp:
+    res *= len(prot_to_rna[aa])
+res *= len(prot_to_rna['Stop'])
+
+print res % 1000000
