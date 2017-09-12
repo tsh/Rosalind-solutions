a9c91270963d90208d9e83e725c9cf7a9f9153ef
Anton Nikulin <tsh@users.noreply.github.com>
2017-09-12 11:46:48 +0300

    Create gc-computing_gc_content.py

---
+++ bioinformatics-stronghold/gc-computing_gc_content.py
@@ -0,0 +1,6 @@
+"""
+Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
+
+Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. 
+Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated.
+"""
