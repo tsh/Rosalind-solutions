57cc61fcab311f6ae8cd05623c09824ee62287c5
Anton Nikulin <tsh@users.noreply.github.com>
2017-09-12 11:56:19 +0300

    Update gc-computing_gc_content.py

--- bioinformatics-stronghold/gc-computing_gc_content.py
+++ bioinformatics-stronghold/gc-computing_gc_content.py
@@ -4,3 +4,10 @@ Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
 Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. 
 Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated.
 """
+dataset = {}
+with open('/vagrant/input.txt') as f:
+    for line in f.readlines():
+        if line.startswith('>'):
+            mark = line[1:-1]
+            dataset[mark] = ''
+    print dataset
