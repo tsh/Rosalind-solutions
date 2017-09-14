fc27b334fc956f2586e6579398c420ff872bd337
Anton Nikulin <tsh@users.noreply.github.com>
2017-09-14 16:15:40 +0300

    Update mprt-finding_a_protein_motif.py

--- bioinformatics-stronghold/mprt-finding_a_protein_motif.py
+++ bioinformatics-stronghold/mprt-finding_a_protein_motif.py
@@ -1,1 +1,7 @@
+"""       
+Given: At most 15 UniProt Protein Database access IDs.
 
+Return: For each protein possessing the N-glycosylation motif, output its given access ID
+followed by a list of locations in the protein string where the motif can be found.
+"""
+base_url = 'http://www.uniprot.org/uniprot/'
